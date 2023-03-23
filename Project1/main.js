269f7d4aaef51976a52b27fd02082282ca2784c9
XXX99XXX <66818824+XXX99XXX@users.noreply.github.com>
2023-03-23 13:54:15 +0200

    Add files via upload

---
+++ Project1/main.js
@@ -0,0 +1,29 @@
+
+let pi = 3.14
+let eu = 1.08
+const username = prompt("Enter your name:"); 
+ 
+console.log(`Hello, dear ${username}!'`);
+
+const Birth = prompt("Enter your date of birth:"); 
+console.log(`So your age is ${2023-Birth}`);
+alert(`So your age is ${2023-Birth}`);
+
+const Radius = prompt("Enter radius of circle:");
+console.log(`So circle area is ${pi*Radius**2}`);
+alert(`So circle area is ${pi*Radius**2}`);
+
+const  distance = prompt("Enter  distance in km between two cities:");
+const time = prompt ("Enter amount of time they have to reach their destination")
+
+console.log(`So speed is ${distance/time}`);
+alert(`So speed is ${distance/time}`);
+
+const  currency = prompt("Enter amount:");
+
+console.log(`So will have ${currency*eu} eur`);
+alert(`So speed is ${currency*eu} eur`);
+
+const  size = prompt("Enter size of flash drive:");
+console.log(`So will have ${parseInt(size*1000/820)} `);
+alert(`So will have  ${parseInt(size*1000/820)} `);
